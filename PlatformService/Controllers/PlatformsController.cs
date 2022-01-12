5bc3a3247da64514dc3cb0b10df3d0c5c7b1eb10
zmmohibul <18-36427-1@student.aiub.edu>
2022-01-12 08:03:48 +0600

    Platform Service - Added Get & Create Controller Action

--- PlatformService/Controllers/PlatformsController.cs
+++ PlatformService/Controllers/PlatformsController.cs
@@ -4,6 +4,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using PlatformService.Data;
 using PlatformService.DTOs;
+using PlatformService.Models;
 
 namespace DefaultNamespace
 {
@@ -26,6 +27,32 @@ namespace DefaultNamespace
             var platforms = await _platformRepository.GetAllPlatforms();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platforms));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public async Task<IActionResult> GetPlatformById(int id)
+        {
+            var platform = await _platformRepository.GetPlatformById(id);
+            if (platform == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(_mapper.Map<PlatformReadDto>(platform));
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreatePlatform(PlatformCreateDto platformCreateDto)
+        {
+            var platform = _mapper.Map<Platform>(platformCreateDto);
+            _platformRepository.CreatePlatform(platform);
+            await _platformRepository.SaveChanges();
+
+            var platformToReturn = _mapper.Map<PlatformReadDto>(platform);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new {Id = platformToReturn.Id}, platformToReturn);
         }
     }
 }
